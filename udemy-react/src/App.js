758605d16976fa476e12dcdbdfbc19b5cae3baa1
AleenaMad <aleena.madni@gmail.com>
2018-05-22 13:29:26 -0500

    understanding state property

--- udemy-react/src/App.js
+++ udemy-react/src/App.js
@@ -3,6 +3,13 @@ import Person from './person/Person.js';
 import './App.css';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'max', age: 28 },
+      { name: 'aleena', age: 24},
+      { name: 'mom', age: 55}
+    ]
+  }
   render() {
     return (
       <div className="App">
@@ -10,11 +17,12 @@ class App extends Component {
         <p className="App-intro">
           hellooooooo
         </p>
+        <button>Switch Name</button>
 
       <div>
-        <Person name="max" age="24" />
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         {/* can put content in between like below and access by props children  in person.js file*/}
-        <Person name="Aleena" age="24" > My Hobbies are: Playing Basketball! </Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > My Hobbies are: Playing Basketball! </Person>
       </div>
       </div>
     );
